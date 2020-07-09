3f6037039540cece0556eef8374c32323de102f8
rafaelgcaldas <rafaelzinhogomes22@live.com>
2020-07-08 23:38:24 -0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from './components/layout';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo} from './styles';
+
+const Main: React.FC = () => {
+  return (
+      <Container>
+          <Header>
+            <button>
+                <BackIcon />
+            </button>
+
+            <ProfileInfo>
+                <strong> Rafael Gomes</strong>
+                <span>999 Tweets</span>
+            </ProfileInfo>
+        </Header>
+
+        {/* <ProfilePage /> */}
+        {/* <BottomMenu>
+            <HomeIcon />
+            <SearchIcon />
+            <BellIcon />
+            <EmailIcon />
+        </BottomMenu> */}
+      </Container>
+  );
+}
+
+export default Main;
