5b202a093957182977fe0a1a0d21e7f25d1c9fde
rafaelgcaldas <rafaelzinhogomes22@live.com>
2020-07-11 22:55:17 -0300

    Feed

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Container, Tab } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+      <Container>
+          <Tab>Tweet</Tab>
+
+          {/* <Tweets>
+              <Tweet />
+              <Tweet />
+              <Tweet />
+              <Tweet />
+              <Tweet />
+          </Tweets> */}
+      </Container>
+  );
+}
+
+export default Feed;
